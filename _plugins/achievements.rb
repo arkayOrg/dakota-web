01270a1ab784ceb24e0f5985ebd92f13ce24da1d
Shade L. Jenifer <shade.jenifer@dakota-consulting.com>
2021-01-18 09:12:14 -0500

    - Added remaining achievements - Adjusted achievements plugin to handle optional properties - Added markup/styling for achievement footnotes

--- _plugins/achievements.rb
+++ _plugins/achievements.rb
@@ -21,15 +21,22 @@ module SiteData
         output << "key: #{achievement['key']}\n"
         output << "layout: achievement\n"
         output << "title: #{title}\n"
-        output << "focus_area: #{achievement['focus_area']}\n"
-        output << "center: #{achievement['center']}\n"
+        output << "focus_area: #{achievement['focus_area']}\n" if achievement['focus_area']
+        output << "center: #{achievement['center']}\n" if achievement['center']
         output << "date: #{achievement['date']}\n"
-        output << "img: #{achievement['img']}\n"
+        output << "img: #{achievement['img']}\n" if achievement['img']
+        output << "learn_more_link: #{achievement['learn_more_link']}\n" if achievement['learn_more_link']
+        output << "description: #{achievement['description']}\n" if achievement['description']
         output << "---\n"
-        output << "<h2><a href=\"site.baseurl/centers/TBD\">{{ page.center }}</a></h2>\n"
-        output << '<p>{{ page.date | date: "%B %d, %Y" }}</p>'
-        output << "\n<p><strong><a href=\"site.baseurl/centers/focus-area/{{ page.focus_area | slugify }}\">{{ page.focus_area }}</a></strong></p>\n\n"
-        output << achievement['content']
+
+        if achievement['learn_more_link']
+          output << "<p><a target=\"_blank\" href=\"{{ page.learn_more_link }}\">Learn more</a></p>"
+        else
+          output << "<h2><a href=\"site.baseurl/centers/TBD\">{{ page.center }}</a></h2>\n"
+          output << '<p>{{ page.date | date: "%B %d, %Y" }}</p>'
+          output << "\n<p><strong><a href=\"site.baseurl/centers/focus-area/{{ page.focus_area | slugify }}\">{{ page.focus_area }}</a></strong></p>\n\n"
+          output << achievement['content']
+        end
 
         mdfilename = File.basename(yml_filename).sub('.yml', '.md')
 
