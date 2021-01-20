790d7cf7bbdae8c108a2d7e2c54a34137452578b
mwangdakota <michael.wang@dakota-consulting.com>
2021-01-20 15:20:50 -0500

    adding js for center directory

---
+++ _assets/js/filters.js
@@ -0,0 +1,239 @@
+
+
+$(document).ready(function(){
+// Add your custom javascript here
+console.log("Hello from michael's script");
+  var filterHeaders=[];
+  var filterHeadersText=[];
+  var filterHeadersInput=[];
+  var filterHeadersInputText=[];
+
+
+if($(".filters__results").length > 0) {
+	//adds filter tags
+    var tags = false;
+    for ( var i = 0, l = filterHeadersInput.length; i < l; i++ ) {
+      tags = true;
+      $('.filters__active ul').append("<li class='inputtrigger' triggerinput='"+ filterHeadersInput[i] + "'>" + filterHeadersInputText[i] + "</li>");
+    }
+
+    for ( var i = 0, l = filterHeaders.length; i < l; i++ ) {  
+      tags = true;
+      $('.filters__active ul').append("<li class='trigger' trigger='"+ filterHeaders[i] + "'>" + filterHeadersText[i] + "</li>");
+    }
+    if(tags)
+    {
+      //add the clear all tag.
+      $('.filters__active ul').append("<li class='clearall'>Clear All</li>");
+    }
+
+    $('.views-exposed-form .usa-search--small button.usa-button').click(function(e){
+      e.preventDefault();
+      $(".js-form-submit").first().click();
+    });
+
+	$('.filters__active li.clearall').click(function() {      
+      $( "input" ).val('');
+      $("input:checkbox").prop('checked', false);
+      $(".js-form-submit").first().click();
+    });
+	$('.filters__active li.inputtrigger').each(function() {      
+      var id = $(this).attr("triggerinput");  
+      $(this).click(function() {
+        $( "input[id^=" + id +"]").val('');
+        $(".js-form-submit").first().click();
+      });
+    });
+	$('.filters__active li.trigger').each(function() {      
+      var id = $(this).attr("trigger");  
+      $(this).click(function() {
+        $( "input[id^=" + id +"]").first().click();
+        $(".js-form-submit").first().click();
+      });
+    });
+
+    filterHeaders = [];
+    filterHeadersText = [];
+    filterHeadersInput = [];
+    filterHeadersInputText = [];
+
+	function createTags() {
+      var dataLayer = [];
+      filterHeaders = [];
+      filterHeadersText = [];
+      filterHeadersInput = [];
+      filterHeadersInputText = [];
+      if($("input[id^=edit-field-year-value]" ).first().val()) {
+        filterHeadersInput.push($("input[id^=edit-field-year-value]" ).first().attr("id").split("--")[0]);
+        filterHeadersInputText.push("After: " + $("input[id^=edit-field-year-value]" ).first().val());
+
+        dataLayer.push({
+          'event': 'directory_search', 
+          'tag': "After: " + $("input[id^=edit-field-year-value]" ).first().val(),
+        });
+      }
+      if($("input[id^=edit-field-year-value-1]").val()) {
+        filterHeadersInput.push($("input[id^=edit-field-year-value-1]").attr("id").split("--")[0]);
+        filterHeadersInputText.push("Before: " + $("input[id^=edit-field-year-value-1]").val());
+
+        dataLayer.push({
+          'event': 'directory_search', 
+          'tag': "Before: " + $("input[id^=edit-field-year-value-1]").val(),
+        });
+      }
+      if($("input[id^=edit-search-api-fulltext]").val()) {
+        filterHeadersInput.push($("input[id^=edit-search-api-fulltext]").attr("id").split("--")[0]);
+        filterHeadersInputText.push( "Search: " + $("input[id^=edit-search-api-fulltext]").val());
+
+        dataLayer.push({
+          'event': 'directory_search', 
+          'tag': "Search: " + $("input[id^=edit-search-api-fulltext]").val(),
+        });
+      }
+      
+      $( "input[type='checkbox']" ).each(function() {
+        //go through the entire list, check what's checked, add it to the array.
+        if($(this).is(":checked")) {
+          filterHeaders.push($(this).attr("id").split("--")[0]);
+          filterHeadersText.push($(this).parent().find('label').html());
+          dataLayer.push({
+            'event': 'directory_search', 
+            'tag': $(this).parent().find('label').html(),
+          });
+        }
+
+        //console.log(dataLayer)
+      });
+    }
+
+
+	//actual filtering code
+   //filter on name
+
+	$rows = $(".filters__results .centers .row:visible");
+    $input = $('#search-fulltext');
+	$($input).keyup(function () {
+	    
+		filterAll();
+	});
+	$year1 = $(".after input");
+	$year2 = $(".before input");
+
+	$($year1).keyup(function () {
+	    
+		filterAll();
+	});
+
+
+	$($year2).keyup(function () {
+	    
+		filterAll();
+	});
+	$focuses = $("#edit-field-focus input");
+	$universities = $("#edit-field-universities input");
+	$locations = $("#edit-field-states input");
+
+	$($focuses).change(function () {
+		filterAll();
+	});
+
+	$($locations).change(function () {
+		filterAll();
+	});
+
+	$($universities).change(function () {
+		filterAll();
+	});
+	function filterAll(){
+		$(".filters__results .centers .row").show();
+
+		$rows = $(".filters__results .centers .row:visible");
+	    
+	    $filter = $input.val().toUpperCase();
+	    $.each($rows, function(i, val){
+	        $name = $(val).find(".title").text();
+	        console.log($name.toUpperCase() + " " + $filter);
+	        if ($name.toUpperCase().indexOf($filter) >= 0) 
+	            $(val).show();
+	        else
+	            $(val).hide();
+	    });
+
+	    if(parseInt($year1.val())>0)
+	    {
+	    	$rows = $(".filters__results .centers .row:visible");
+		    $filter = $year1.val();
+		    $.each($rows, function(i, val){
+		        $year = $(val).find(".year").text();
+		        console.log($year+ " " + $filter + ($year >= $filter));
+		        if (parseInt($year) >= parseInt($filter)) 
+		            $(val).show();
+		        else
+		            $(val).hide();
+			});
+		}
+
+	    if(parseInt($year2.val())>0)
+	    {
+	    	$rows = $(".filters__results .centers .row:visible");
+			$filter = $year2.val();
+		    $.each($rows, function(i, val){
+		        $year = $(val).find(".year").text();
+		        console.log($year + " " + $filter );
+		        if (parseInt($year) <= parseInt($filter)) 
+		            $(val).show();
+		        else
+		            $(val).hide();
+		    });
+	    }
+	    $rows = $(".filters__results .centers .row:visible");
+
+		$.each($universities, function(i, foc){
+	        if ($(this).is(':checked')) {
+	        	$filter = $(foc).parent().find("label").text().toUpperCase();
+    			$.each($rows, function(j, val){
+			        $university = $(val).find(".universities").text();
+			        console.log($university + " " + $filter );
+			        if ($university.toUpperCase().indexOf($filter) >= 0) 
+			            $(val).show();
+			        else
+			            $(val).hide();
+		    	});
+  			}
+	    });
+		
+	    $rows = $(".filters__results .centers .row:visible");
+
+	    $.each($locations, function(i, foc){
+	        if ($(this).is(':checked')) {
+	        	$filter = $(foc).parent().find("label").text().toUpperCase();
+    			$.each($rows, function(j, val){
+			        $location = $(val).find(".location").text();
+			        console.log($focus + " " + $filter );
+			        if ($location.toUpperCase().indexOf($filter) >= 0) 
+			            $(val).show();
+			        else
+			            $(val).hide();
+		    	});
+  			}
+	    });
+	    
+	    $rows = $(".filters__results .centers .row:visible");
+
+	    $.each($focuses, function(i, foc){
+	        if ($(this).is(':checked')) {
+	        	$filter = $(foc).parent().find("label").text().toUpperCase();
+    			$.each($rows, function(j, val){
+			        $focus = $(val).find(".focus").text();
+			        console.log($focus + " " + $filter );
+			        if ($focus.toUpperCase().indexOf($filter) >= 0) 
+			            $(val).show();
+			        else
+			            $(val).hide();
+		    	});
+  			}
+	    });
+		createTags();
+	}
+}
+});
---
+++ _pages/centertest2.md
@@ -0,0 +1,79 @@
+---
+title: centertest
+acronym: WGRC
+layout: page
+sidenav: false
+---
+
+{% assign center = site.data.centers[page.acronym] %}
+<dir></dir>
+
+<div class="hero">
+  <div id="carouselCenterInd" class="carousel slide carousel--interior" data-ride="carousel" data-pause="false" data-interval="false">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="container">
+          <div class="carousel__content carousel__content--slant">
+            <div class="carousel__text-cta">
+              <nav class="breadcrumbs" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Centers</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">{{ center.title }}</li>
+                </ol>
+              </nav>
+              <h1>{{ center.title }}</h1>
+              <p class="carousel__subtitle">Last Reviewed: 07/19/17</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="content-block">
+    <div class="container">
+      <div class="content-block__inner" style="font-style:italic">The opinions, findings, and conclusions or recommendations expressed are those of the Center author(s) and do not necessarily reflect the views of the National Science Foundation.
+      </div>
+    </div>
+  </div>
+  <div class="content-block">
+    <div class="container">
+      <div class="content-block__inner">
+        <h2 id=""><span class="highlight">Center Overview</span></h2>
+          {{ center.field_overview }}
+      </div>
+    </div>
+  </div>
+{% assign universities = center.field_universities | split: ", " %}
+
+<div class="content-block">
+  <div class="container">
+    <div class="content-block__inner">
+        <h2 id=""><span class="highlight">Universities</span></h2>
+          <ul>
+            {% for member in universities %}
+              <li>{{ member }}</li>
+            {% endfor %}
+          </ul>
+          <a class="usa-button btn-tertiary" href="http://www.ambic.org">View Center Website</a>
+    </div>
+  </div>
+</div>
+<div class="content-block content-block--bg">
+    <div class="container">
+      <div class="content-block__inner">
+        <h2 id=""><span class="highlight">Research Focus</span></h2>
+{{ center.field_research_focus }}
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="content-block__inner">
+          <h2 id=""><span class="highlight">Awards</span></h2>
+          <div class="content-block__columns">
+            <p>
+        <a href="https://www.nsf.gov/awardsearch/advancedSearchResult?AwardNumberOperator=Exactly&amp;AwardNumberFrom=1624641&amp;ActiveAwards=true&amp;ExpiredAwards=true">1624641</a>
+    </p>
+          </div>
+        </div>
+      </div>
